71d9687152db4c5e9c8ac259ec99d4e048bff215
Barry Percy <baz.percy@gmail.com>
2022-11-30 15:58:07 +0000

    homework completed

---
+++ Objects, Arrays Loops (EXTRA).js
@@ -0,0 +1,106 @@
+// Additional assignments for Day 8
+
+/* EXTRA 1
+ Write a piece of code for reverting an array.
+ es:
+ [1, 3, 5] ==> [5, 3, 1]
+*/
+let arrayToReverse = [1,3,5,7,9,2,4];
+for(i=0;i<parseInt(arrayToReverse.length/2);i++){
+    temp = arrayToReverse[i];
+    arrayToReverse[i]=arrayToReverse[arrayToReverse.length-i-1];
+    arrayToReverse[arrayToReverse.length-i-1]=temp;
+}
+console.log(arrayToReverse);
+
+
+
+/* EXTRA 2
+ Write a piece of code for getting the maximum numerical value from an array.
+*/
+
+let findLargest = [1,5,22,3];
+let biggest = findLargest[0];
+for(i=1;i<findLargest.length;i++){
+    if(findLargest[i]>biggest){
+        biggest = findLargest[i];
+    }
+}
+console.log(biggest);
+
+/* EXTRA 3
+ Write a piece of code for getting the minimum numerical value from an array.
+*/
+
+let findSmallest = [1,5,22,3];
+let smallest = findSmallest[0];
+for(i=1;i<findSmallest.length;i++){
+    if(findSmallest[i]<smallest){
+        smallest = findSmallest[i];
+    }
+}
+console.log(smallest);
+
+/* EXTRA 4
+ Write a piece of code for getting only even numerical values from an array.
+*/
+
+let findingArray = [0,1,2,3,4,5,6,7,8,9,10];
+let evenNumbers = [];
+
+for(i=0;i<findingArray.length;i++){
+    if(findingArray[i]%2===0){
+        evenNumbers.push(findingArray[i]);
+    }
+}
+console.log(evenNumbers);
+
+/* EXTRA 5
+ Write a piece of code for deleting only even entries from an array.
+*/
+
+let deleteFromArray = [0,1,2,3,4,5,6,7,8,9,10,32,34,36,38,40,12,14,16,18,20,11,13];
+for(i=0;i<deleteFromArray.length;i++){
+    if(deleteFromArray[i]%2===0){
+        deleteFromArray.splice(i,1);
+    }
+}
+console.log(deleteFromArray);
+
+
+/* EXTRA 6
+ Write a piece of code for removing all the vowels from a string.
+*/
+let removeFromString = "blaeadsaodasdjuio"
+let lengthOfString = removeFromString.length;
+for(i=0;i<lengthOfString;i++){
+    removeFromString = removeFromString.replace('a','');
+    removeFromString = removeFromString.replace('e','');
+    removeFromString = removeFromString.replace('i','');
+    removeFromString = removeFromString.replace('o','');
+    removeFromString = removeFromString.replace('u','');
+}
+console.log("letters remaining after removal:",removeFromString);
+
+
+/* EXTRA 7
+ Write a piece of code for increasing all the numerical values in a array by 1.
+*/
+let increaseArray = [0,1,2,3,4];
+for(i=0;i<increaseArray.length;i++){
+    increaseArray[i] +=1;
+}
+console.log(increaseArray);
+
+/* EXTRA 8 
+ Replace all the strings contained in an array with their length.
+ es.: ["strive", "is", "great"] => [6, 2, 5]
+*/
+
+let stringArray = ["sup bro", "hey", "a"];
+
+for(let i=0;i<stringArray.length;i++){
+    stringArray[i] = stringArray[i].length;
+}
+
+console.log(stringArray);
---
+++ Objects, Arrays Loops.js
@@ -0,0 +1,179 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- You can Google / use StackOverflow BUT only when you think you need something we didn't cover during lessons yet
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+- You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+- The solution must be available for the tutors by the end of the day (5PM CET)
+*/
+
+
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 2
+ Create a variable and assign to it an object containing your name, surname, email address and age.
+*/
+
+const person = {
+	name: "Barry", // string property
+	surname: "Rambo", // another string property
+    email: "example@example.com",
+	age: 29 // numeric property
+};
+
+console.log(person.name);
+
+/* EXERCISE 3
+ Add to the previously created object a property with a boolean value to rappresent wheter you have or not a driving license.
+*/
+
+person.hasLicense = true;
+
+console.log(person.hasLicense);
+
+/* EXERCISE 4
+ Remove from the previously created object the age property.
+*/
+
+delete person.age;
+
+console.log(person.age);
+
+/* EXERCISE 5
+ Create a second object with another name, surname, email address and verify that this object has a different email address than the previous one.
+*/
+
+const person2 = {
+	name: "Billy", // string property
+	surname: "Bob", // another string property
+    email: "example2@example.com",
+};
+
+console.log("Person 1 has email:", person.email,"\nPerson 2 has email:",person2.email);
+
+/* EXERCISE 6
+ You are working on an e-commerce website. In the variable totalShoppingCart you are storing the total amount spent by the current user.
+ Currently you have a promotion: if the customer's shopping cart total is more than 50, the user is eligible for free shipping (otherwise it costs 10).
+ Write an algorithm that calculates the total cost to charge the user with.
+*/
+
+let totalShoppingCart = 62;
+let finalPrice;
+
+if(totalShoppingCart>50){
+    finalPrice = totalShoppingCart;
+}else{
+    finalPrice = totalShoppingCart+10;
+}
+console.log(finalPrice);
+
+/* EXERCISE 7
+ You are working on an e-commerce website. Today is Black Friday and everything has a 20% discount at the end of the purchase.
+ Modify the previous answer inserting this information and, applying the same rules for the shipping cost, calculate the totalCost.
+*/
+
+let blackFridayPrice = totalShoppingCart*(80/100);
+
+if(blackFridayPrice>50){
+    finalPrice = blackFridayPrice;
+}else{
+    finalPrice = blackFridayPrice+10;
+}
+console.log(finalPrice);
+
+/* EXERCISE 8
+ Create a variable and assign to it an object representing a car, with properties like brand, model and licensePlate.
+ Then clone it 5 times, and change the licensePlate for each cloned car without affecting the original one.
+*/
+
+const car1 = {
+	brand: "Subaru", // string property
+	model: "supafast", // another string property
+    licensePlate: "12345",
+};
+let car2={};
+let car3={};
+let car4={};
+let car5={};
+let car6={};
+
+Object.assign(car2,car1);
+Object.assign(car3,car1);
+Object.assign(car4,car1);
+Object.assign(car5,car1);
+Object.assign(car6,car1);
+
+car2.licensePlate = "23456";
+car3.licensePlate = "34567";
+car4.licensePlate = "45678";
+car5.licensePlate = "56789";
+car6.licensePlate = "67890";
+
+console.log(car1.licensePlate);
+console.log(car2.licensePlate);
+console.log(car3.licensePlate);
+console.log(car4.licensePlate);
+console.log(car5.licensePlate);
+console.log(car6.licensePlate);
+
+/* EXERCISE 9
+ Create a variable called carsForRent and assign to it an array containing all the cars from the previous exercise.
+*/
+
+let carsForRent = [car1,car2,car3,car4,car5,car6];
+
+/* EXERCISE 10
+ Remove the first and the last car from the carsForRent array.
+*/
+
+carsForRent.shift();
+carsForRent.pop();
+
+for(let i=0;i<carsForRent.length;i++){
+    console.log(carsForRent[i]);
+}
+
+/* EXERCISE 11
+ Print to the console the type of the car variable you created before, as well as the types of its licensePlate and brand properties.
+*/
+
+console.log(typeof car1);
+console.log(typeof car1.licensePlate);
+console.log(typeof car1.brand);
+
+/* EXERCISE 12
+ Create a new variable called carsForSale assigning to it an empty array, and then insert 3 cars into it.
+ Create a new variable called totalCars and assign to it the total number of cars present in the carsForSale and carsForRent arrays.
+*/
+
+let carsForSale = [];
+let car7={
+    brand: "Subaru", // string property
+	model: "supafast", // another string property
+    licensePlate: "98765",
+};
+let car8={
+    brand: "Subaru", // string property
+	model: "supafast", // another string property
+    licensePlate: "87654",
+};
+let car9={
+    brand: "Subaru", // string property
+	model: "supafast", // another string property
+    licensePlate: "76543",
+};
+
+carsForSale.push(car7,car8,car9);
+
+totalCars = carsForSale.length+carsForRent.length;
+console.log(totalCars);
+
+/* EXERCISE 13
+ Using a loop, print to the console all the data for each car in the carsForSale array.
+*/
+
+for(let i=0;i<carsForSale.length;i++){
+    console.log(carsForSale[i]);
+}
